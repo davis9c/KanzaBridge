d1e732623521dd4d79e0957560136b1f5f27eed1
Davis Aji <dvisboy@gmail.com>
2026-02-19 13:47:18 +0700

    uselessfile

--- public/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-Type: application/json');
-echo json_encode([
-    'php_input' => file_get_contents('php://input'),
-    'post' => $_POST,
-    'server_ct' => $_SERVER['CONTENT_TYPE'] ?? null
-]);
